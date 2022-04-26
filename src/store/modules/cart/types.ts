67b262d6bd3279b6652d1988d4d6f147612929ba
Samara Carvalho <samcarvalhos@hotmail.com>
2022-04-26 14:40:29 -0300

    Reducer initial state

---
+++ src/store/modules/cart/types.ts
@@ -0,0 +1,15 @@
+export interface IProduct {
+    id: string;
+    title: string;
+    price: number;
+}
+
+
+export interface ICartItem {
+    product: IProduct;
+    quantity: number
+}
+
+export interface ICartState {
+    items: ICartItem[];
+}
