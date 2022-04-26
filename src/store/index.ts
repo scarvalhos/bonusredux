d7446839cacffa956d27a6f2fec7b229a96472e0
Samara Carvalho <samcarvalhos@hotmail.com>
2022-04-26 17:55:51 -0300

    redux-saga configuration

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,27 @@
-import { legacy_createStore as createStore } from 'redux'
+import { legacy_createStore as createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import rootReducer from './modules/rootReducer'
 import { ICartState } from './modules/cart/types'
+import rootSaga from './modules/rootSaga'
 
 export interface IState {
     cart: ICartState;
 }
 
+const sagaMiddleware = createSagaMiddleware()
+
+const middlewares = [sagaMiddleware]
+
 const store = createStore(
     rootReducer,
-    composeWithDevTools()
+    composeWithDevTools(
+        applyMiddleware(...middlewares)
+    )
 )
+
+sagaMiddleware.run(rootSaga)
 
 export default store
