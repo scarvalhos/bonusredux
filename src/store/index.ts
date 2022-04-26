b5e699e31431a28654c69e43aaa2b8dfe53595db
Samara Carvalho <samcarvalhos@hotmail.com>
2022-04-26 15:51:00 -0300

    Redux types

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,11 @@
 import { legacy_createStore as createStore } from 'redux'
 
 import rootReducer from './modules/rootReducer'
+import { ICartState } from './modules/cart/types'
+
+export interface IState {
+    cart: ICartState;
+}
 
 const store = createStore(rootReducer)
 
