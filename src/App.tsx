c520097b28e89766ea269dc2c93a6b5c4d800afe
Samara Carvalho <samcarvalhos@hotmail.com>
2022-04-26 15:39:47 -0300

    Changing state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Provider } from 'react-redux'
+import Cart from './components/Cart'
 import Catalog from './components/Catalog'
 
 import store from './store'
@@ -7,6 +8,7 @@ function App() {
   return (
     <Provider store={store}>
       <Catalog />
+      <Cart />
     </Provider>
   )
 }
