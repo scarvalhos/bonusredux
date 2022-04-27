61deef715786c7886aa9c7a68eb5d5a15b3ce0c4
Samara Carvalho <samcarvalhos@hotmail.com>
2022-04-27 12:21:51 -0300

    Dividing actions

--- src/store/modules/cart/actions.ts
+++ src/store/modules/cart/actions.ts
@@ -1,11 +1,29 @@
 import { IProduct } from "./types";
 
-export function addProductToCart(product: IProduct) {
+export function addProductToCartRequest(product: IProduct) {
     return {
-        type: 'ADD_PRODUCT_TO_CART',
+        type: 'ADD_PRODUCT_TO_CART_REQUEST',
         // Any additional informations needed
         payload: {
             product
+        }
+    }
+}
+export function addProductToCartSuccess(product: IProduct) {
+    return {
+        type: 'ADD_PRODUCT_TO_CART_SUCCESS',
+        // Any additional informations needed
+        payload: {
+            product
+        }
+    }
+}
+export function addProductToCartFailure(productId: number) {
+    return {
+        type: 'ADD_PRODUCT_TO_CART_FAILURE',
+        // Any additional informations needed
+        payload: {
+            productId
         }
     }
 }
