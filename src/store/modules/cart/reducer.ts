fe041b66d2728f5a709b32be4fe6625d90da0952
Samara Carvalho <samcarvalhos@hotmail.com>
2022-04-27 13:15:06 -0300

    Signaling out of stock

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -4,7 +4,8 @@ import produce from 'immer'
 import { ICartState } from "./types"
 
 const INITIAL_STATE: ICartState = {
-    items: []
+    items: [],
+    failedStockCheck: []
 }
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
@@ -29,7 +30,7 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
                 break;
             }
             case 'ADD_PRODUCT_TO_CART_FAILURE': {
-                console.log('Failure', action.payload)
+                draft.failedStockCheck.push(action.payload.productId)
 
                 break;
             }
--- src/store/modules/cart/types.ts
+++ src/store/modules/cart/types.ts
@@ -12,4 +12,5 @@ export interface ICartItem {
 
 export interface ICartState {
     items: ICartItem[];
+    failedStockCheck: number[];
 }
