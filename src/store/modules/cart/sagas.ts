188f3d452c44da268fd795227a629d4634531121
Samara Carvalho <samcarvalhos@hotmail.com>
2022-04-27 13:09:06 -0300

    finalizing stock check

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,8 +1,16 @@
-import { all, select, takeLatest } from 'redux-saga/effects'
+import { AxiosResponse } from 'axios'
+import { all, call, put, select, takeLatest } from 'redux-saga/effects'
+
+import { addProductToCartFailure, addProductToCartRequest, addProductToCartSuccess } from './actions'
 import { IState } from '../..'
-import { addProductToCartRequest } from './actions'
+import api from '../../../services/api'
 
 type CheckProductStockRequest = ReturnType<typeof addProductToCartRequest>
+
+interface IStokeResponse {
+    id: number;
+    quantity: number;
+}
 
 function* checkProductStock({ payload }: CheckProductStockRequest) {
     const { product } = payload
@@ -11,7 +19,13 @@ function* checkProductStock({ payload }: CheckProductStockRequest) {
         return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0
     })
 
-    console.log(currentQuantityInCart)
+    const availableStockResponse: AxiosResponse<IStokeResponse> = yield call(api.get, `stock/${product.id}`)
+
+    if(availableStockResponse.data.quantity > currentQuantityInCart) {
+        yield put(addProductToCartSuccess(product))
+    } else {
+        yield put(addProductToCartFailure(Number(product.id)))
+    }
 }
 
 export default all([
